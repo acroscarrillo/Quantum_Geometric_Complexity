cb7ef5eb0cc3143f2618bacf81e1f7055d81b276
Alex's mac <a.croscarrillo@gmail.com>
2025-01-24 00:34:35 +0100

    fixed!

---
+++ docs/src/experiments/exp_2.jl
@@ -0,0 +1,17 @@
+# # [exp.jl exp](@id exp)
+
+
+# ### An interesting scatter
+# wow what a scatter!
+using Pkg
+Pkg.instantiate()
+
+using Plots
+
+scatter(rand(100,2))
+
+
+
+# ### A rather puzzling heatmap
+# This is a heatmap of a random matrix
+heatmap(randn(100,100))
---
+++ docs/src/experiments/exp_3.jl
@@ -0,0 +1,21 @@
+# # [exp.jl exp](@id exp)
+
+
+# ### An interesting exponential
+# wow what a growth
+using Pkg
+Pkg.instantiate()
+
+using Plots
+
+scatter(exp.(1:10))
+plot!(exp.(1:10))
+
+
+
+
+# ### A relaxing sine and cosine
+# This are awesone sinusoidal functions
+
+plot(sin.(-3:0.01:3))
+plot(cos.(-3:0.01:3))
