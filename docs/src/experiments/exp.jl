5a2137199edfbcbc1f6402c4cc412e111645b711
Alex's mac <a.croscarrillo@gmail.com>
2025-01-23 19:05:27 +0100

    it works!!

--- docs/src/experiments/exp.jl
+++ docs/src/experiments/exp.jl
@@ -1,10 +1,16 @@
 # # [exp.jl exp](@id exp)
 
 
-# ### Test plot
+# ### An interesting plot
 using Pkg
 Pkg.instantiate()
 
 using Plots
 
 plot(rand(100,2))
+
+
+
+# ### A rather boring normal
+
+histogram(randn(10000,1))
