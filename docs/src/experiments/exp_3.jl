2b470c4530bbf7a44fc63ef55fd7ea6c9710f628
Alex's mac <a.croscarrillo@gmail.com>
2025-01-24 01:05:19 +0100

    added LaTeX

--- docs/src/experiments/exp_3.jl
+++ docs/src/experiments/exp_3.jl
@@ -12,6 +12,13 @@ plot!(exp.(1:10))
 
 # ### A relaxing sine and cosine
 # This are awesone sinusoidal functions
+# Here are the functions in LaTeX:
+
+# ```math
+# \cos(x) \quad \sin(x)
+# ```
+
+# This is lovely.
 
 plot(sin.(-3:0.01:3))
 plot!(cos.(-3:0.01:3))
